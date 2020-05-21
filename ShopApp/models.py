d64452d1340993d1b82e139d4c5cde44af58b665
me <ilya.kashapov99@gmail.com>
2020-05-22 01:20:07 +0500

    create product model

--- ShopApp/models.py
+++ ShopApp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    count = models.IntegerField(null=False)
+    img = models.ImageField(upload_to='products', blank=True, null=True)
+    description = models.CharField(null=False, max_length = 100)
